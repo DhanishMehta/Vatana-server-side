e4417d6d6cb2cb5028d242258881beb3d33b9f42
Dhanish Mehta <dhanishmehta47@gmail.com>
2023-11-19 23:14:31 +0530

    Initial commit

---
+++ src/main/java/com/capstone/grocery/controller/CategoryController.java
@@ -0,0 +1,45 @@
+package com.capstone.grocery.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capstone.grocery.model.CategoryOfTree;
+import com.capstone.grocery.response.CommonResponse;
+import com.capstone.grocery.service.CategoryService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("/{id}")
+    public CommonResponse<CategoryOfTree> getCategory(@PathVariable Integer id){
+        return categoryService.getCategory(id);
+    }
+
+    @PostMapping
+    public CommonResponse<CategoryOfTree> postCategory(@RequestBody CategoryOfTree category){
+        return categoryService.postCategory(category);
+    }
+
+    @GetMapping("/tree")
+    public CommonResponse<List<CategoryOfTree>> getCategoryTree(){
+        return categoryService.getCategoryTree();
+    }
+
+    @PostMapping("/tree")
+    public CommonResponse<List<CategoryOfTree>> postCategoryTree(@RequestBody List<CategoryOfTree> categoryList){
+        return categoryService.postCategoryTree(categoryList);
+    }
+}
